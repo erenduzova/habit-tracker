20c2a95ccffae5ea4e7ee42b3499b21a04694cef
Eren Düzova <erenduzova@gmail.com>
2022-09-23 00:23:35 +0300

    POST Graph
    Parameters set for graph.
    POST request made to pixela.

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ load_dotenv()
 USERNAME = os.getenv("USER_NAME")
 TOKEN = os.getenv("PIXELA_TOKEN")
 
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
 # User Parameters
 USER_PARAMS = {
     "token": TOKEN,
@@ -15,9 +19,23 @@ USER_PARAMS = {
     "notMinor": "yes"
 }
 
+# Graph Parameters
+GRAPH_PARAMS = {
+    "id": "graph-water1",
+    "name": "Drinking Water Graph",
+    "unit": "L",
+    "type": "float",
+    "color": "sora"
+}
+
 # Endpoints
 pixela_endpoint = "https://pixe.la/v1/users"
+graphs_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs"
 
 # Post User
-response = requests.post(url=pixela_endpoint, json=USER_PARAMS)
-print(response.text)
+response_user = requests.post(url=pixela_endpoint, json=USER_PARAMS)
+print(response_user.text)
+
+# Post Graph
+response_gr = requests.post(url=graphs_endpoint, json=GRAPH_PARAMS, headers=headers)
+print(response_gr.text)
