80d6b0b265cce06cf98a88ad02a874c116dbb0bd
Eren Düzova <erenduzova@gmail.com>
2022-09-22 23:46:23 +0300

    Set Pixela User
    Parameters set for user.
    POST request made to pixela.

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ import os
 import requests
 
 load_dotenv()
+
+USERNAME = os.getenv("USER_NAME")
+TOKEN = os.getenv("PIXELA_TOKEN")
+
+# User Parameters
+USER_PARAMS = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# Endpoints
+pixela_endpoint = "https://pixe.la/v1/users"
+
+# Post User
+response = requests.post(url=pixela_endpoint, json=USER_PARAMS)
+print(response.text)
