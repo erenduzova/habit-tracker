13ab4c0f9ad409472fdfdb39988ea6f3aa566d96
Eren Düzova <erenduzova@gmail.com>
2022-09-23 16:10:16 +0300

    Add Put Pixel
    Put Pixel request added and previous code commented.
    Because we already created pixel.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 import requests
-from datetime import date
+from datetime import date, datetime
 
 load_dotenv()
 
@@ -10,6 +10,9 @@ TOKEN = os.getenv("PIXELA_TOKEN")
 
 # Date
 TODAY = date.today()
+
+# Change year, month, day to date you want to update
+UPDATE_DAY = datetime(year=2022, month=9, day=23).strftime("%Y%m%d")
 
 headers = {
     "X-USER-TOKEN": TOKEN
@@ -39,10 +42,16 @@ POST_PIXEL_PARAMS = {
     "quantity": "1.6"  # 8 glass of water
 }
 
+# Put Pixel Parameters
+PUT_PIXEL_PARAMS = {
+    "quantity": "1.8" # Add 1 glass more and update
+}
+
 # Endpoints
 pixela_endpoint = "https://pixe.la/v1/users"
 graphs_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs"
 post_pixel_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_PARAMS['id']}"
+put_pixel_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_PARAMS['id']}/{UPDATE_DAY}"
 
 # # Post User
 # response_user = requests.post(url=pixela_endpoint, json=USER_PARAMS)
@@ -52,6 +61,10 @@ post_pixel_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_PARAMS
 # response_gr = requests.post(url=graphs_endpoint, json=GRAPH_PARAMS, headers=headers)
 # print(response_gr.text)
 #
-# Post Pixel
-response_px = requests.post(url=post_pixel_endpoint, json=POST_PIXEL_PARAMS, headers=headers)
-print(response_px.text)
+# # Post Pixel
+# response_px = requests.post(url=post_pixel_endpoint, json=POST_PIXEL_PARAMS, headers=headers)
+# print(response_px.text)
+
+# Update Pixel
+response_up_px = requests.put(url=put_pixel_endpoint, json=PUT_PIXEL_PARAMS, headers=headers)
+print(response_up_px.text)
