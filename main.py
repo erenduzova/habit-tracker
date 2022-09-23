0a383f0cd31128d409a1b956b25138a665125a13
Eren Düzova <erenduzova@gmail.com>
2022-09-23 15:57:22 +0300

    Add Post Pixel
    Post Pixel request added and previous code commented.
    Because we already created user and graph.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from dotenv import load_dotenv
 import os
 import requests
+from datetime import date
 
 load_dotenv()
 
 USERNAME = os.getenv("USER_NAME")
 TOKEN = os.getenv("PIXELA_TOKEN")
+
+# Date
+TODAY = date.today()
 
 headers = {
     "X-USER-TOKEN": TOKEN
@@ -29,14 +33,25 @@ GRAPH_PARAMS = {
     "timezone": "Turkey"
 }
 
+# Pixel Post Parameters
+POST_PIXEL_PARAMS = {
+    "date": TODAY.strftime("%Y%m%d"),
+    "quantity": "1.6"  # 8 glass of water
+}
+
 # Endpoints
 pixela_endpoint = "https://pixe.la/v1/users"
 graphs_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs"
+post_pixel_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_PARAMS['id']}"
 
-# Post User
-response_user = requests.post(url=pixela_endpoint, json=USER_PARAMS)
-print(response_user.text)
-
-# Post Graph
-response_gr = requests.post(url=graphs_endpoint, json=GRAPH_PARAMS, headers=headers)
-print(response_gr.text)
+# # Post User
+# response_user = requests.post(url=pixela_endpoint, json=USER_PARAMS)
+# print(response_user.text)
+#
+# # Post Graph
+# response_gr = requests.post(url=graphs_endpoint, json=GRAPH_PARAMS, headers=headers)
+# print(response_gr.text)
+#
+# Post Pixel
+response_px = requests.post(url=post_pixel_endpoint, json=POST_PIXEL_PARAMS, headers=headers)
+print(response_px.text)
